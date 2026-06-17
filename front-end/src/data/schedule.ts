894a837749864f05a5a767250a32a1f9ae4a2b17
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-06-17 16:13:42 -0300

    Feature: Adicionando dados de grade horária.

---
+++ front-end/src/data/schedule.ts
@@ -0,0 +1,28 @@
+import type { AulaHorario } from '../types';
+
+export const grade: AulaHorario[] = [
+    {
+        horario: '07:00',
+        segunda: 'Karate',
+        terca: 'Kickboxing',
+        quarta: 'Karate',
+        quinta: 'Kickboxing',
+        sexta: 'Karate',
+    },
+    {
+        horario: '12:00',
+        segunda: 'Jiu-Jitsu',
+        terca: 'Condicionamento',
+        quarta: 'Jiu-Jitsu',
+        quinta: 'Condicionamento',
+        sexta: 'Jiu-Jitsu',
+    },
+    {
+        horario: '19:00',
+        segunda: 'Kickboxing',
+        terca: 'Jiu-Jitsu',
+        quarta: 'Kickboxing',
+        quinta: 'Jiu-Jitsu',
+        sexta: 'Kickboxing',
+    },
+];
