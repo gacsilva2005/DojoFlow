aa3343e3a86c8cb985119eb02c039ce97c469e85
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-06-17 15:35:36 -0300

    Feature: Dados mockados dentro da sessão dos professores oferecidas pela academia.

---
+++ front-end/src/data/professores.ts
@@ -0,0 +1,28 @@
+import type { Professor } from '../types';
+
+export const professores: Professor[] = [
+    {
+        id: 'sensei-luciano',
+        nome: 'Sensei Luciano',
+        titulo: 'Faixa preta 5º Dan',
+        imagem: '/src/assets/images/sensei-luciano.jpg',
+    },
+    {
+        id: 'prof-felipe',
+        nome: 'Prof. Felipe',
+        titulo: 'Faixa preta 3º Dan',
+        imagem: '/src/assets/images/prof-felipe.jpg',
+    },
+    {
+        id: 'prof-marcelo',
+        nome: 'Prof. Marcelo',
+        titulo: 'Faixa preta 2º Dan',
+        imagem: '/src/assets/images/prof-marcelo.jpg',
+    },
+    {
+        id: 'prof-rafael',
+        nome: 'Prof. Rafael',
+        titulo: 'Faixa preta 1º Dan',
+        imagem: '/src/assets/images/prof-rafael.jpg',
+    },
+];
